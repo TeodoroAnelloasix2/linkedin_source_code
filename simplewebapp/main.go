a621217db234ac8c3fd6506e550fba204e1d78a0
TeodoroAnelloasix2 <giacomoeneldo@gmail.com>
2025-12-07 15:23:50 +0100

    kkk

--- simplewebapp/main.go
+++ simplewebapp/main.go
@@ -21,9 +21,17 @@ func SayHello(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func SayBye(w http.ResponseWriter, r *http.Request) {
+	msg := "Good bye"
+	_, err := w.Write([]byte(msg))
+	if err != nil {
+		log.Fatalf("error running bye func: %v", err)
+	}
+}
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/hello", SayHello)
+	http.HandleFunc("/bye", SayBye)
 	log.Fatal(http.ListenAndServe(":8085", nil))
 
 }
